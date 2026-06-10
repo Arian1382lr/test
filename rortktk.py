b65aba97fb5abb5b746f0f5bce77625b9dae7754
Arian <arian1332@gmail.com>
2026-06-10 17:31:59 +0330

    change code py

--- rortktk.py
+++ rortktk.py
@@ -1,16 +1,20 @@
 a=12
 b=13
 c=a+b
-print(c);
-q=["ali","mamad","hossein"];
-print(q[2]);
+print(c)
+q=["ali","mamad","hossein"]
+q.append("aa7")
+print(q[1:2])
 
-aa=input("enter your name");
-ab=input("enter your pasword");
 
-if{
-aa ==("ali") and ab=="mamad"
-print("you log");
-else
-print("you not log");
-}
+aa=input("enter your name")
+ab=input("enter your pasword")
+
+if aa ==('ali') and ab==('mamad'):
+
+  print('you log')
+else:
+  print('you not log')
+
+
+
