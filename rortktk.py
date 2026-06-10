76859c69dc7058739f1b28dc8ee0a0c6cf706dda
Arian <arian1332@gmail.com>
2026-06-10 15:24:31 +0330

    pyton mohem

--- rortktk.py
+++ rortktk.py
@@ -2,3 +2,15 @@ a=12
 b=13
 c=a+b
 print(c);
+q=["ali","mamad","hossein"];
+print(q[2]);
+
+aa=input("enter your name");
+ab=input("enter your pasword");
+
+if{
+aa ==("ali") and ab=="mamad"
+print("you log");
+else
+print("you not log");
+}
